d2c3ce6b0194e7085a4954a957ac959aa0d8b47e
tnkryoga <tanaryo-miyabi818@outlook.jp>
2023-06-01 15:59:18 +0900

    feat:newmodel

--- exp_1/main.py
+++ exp_1/main.py
@@ -1,94 +1,92 @@
 import os
-
 import hydra
 import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
 import torch.optim as optim
 from omegaconf import DictConfig
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 from sklearn.model_selection import train_test_split
+from torch import nn
 from torch.utils.data import DataLoader, Dataset
-from torchmetrics.functional import accuracy, auroc
-from transformers import BertModel, BertTokenizer
+from transformers import BertModel
+from transformers.tokenization_bert_japanese import BertJapaneseTokenizer
 
 
-class BertDataset(Dataset):
-    def __init__(
-        self,
-        df: pd.DataFrame,
-        tokenizer: BertTokenizer,
-        max_length: int,
-        text_column_name: str,
-        label_column_name: str,
-    ):
-        self.df = df
+# Dataset
+class CreateDataset(Dataset):  # 文章のtokenize処理を行ってDataLoaderに渡す関数
+    TEXT_COLUMN = "sentence"
+    LABEL_COLUMN = "label"
+
+    def __init__(self, data, tokenizer, max_token_len):
+        self.data = data
         self.tokenizer = tokenizer
-        self.max_length = max_length
-        self.text_columm_name = text_column_name
-        self.label_column_name = label_column_name
+        self.max_token_len = max_token_len
 
     def __len__(self):
-        return len(self.df)
+        return len(self.data)
 
-    def __getitem__(self, index: int):
-        df_row = self.df.iloc[index]
-        text = df_row[self.text_columm_name]
-        labels = df_row[self.label_column_name].astype(int)
+    def __getitem__(self, index):
+        data_row = self.data.iloc[index]
+        text = data_row[self.TEXT_COLUMN]
+        labels = data_row[self.LABEL_COLUMN]
 
         encoding = self.tokenizer.encode_plus(
             text,
             add_special_tokens=True,
-            max_length=self.max_length,
+            max_length=self.max_token_len,
             padding="max_length",
             truncation=True,
-            return_token_type_ids=False,
-            return_attention_mask=False,
+            return_attention_mask=True,
             return_tensors="pt",
         )
 
-        return encoding["input_ids"].flatten(), torch.tensor(labels)
+        return dict(
+            text=text,
+            input_ids=encoding["input_ids"].flatten(),
+            attention_mask=encoding["attention_mask"].flatten(),
+            labels=torch.tensor(labels),
+        )
 
 
+# DataModule
 class CreateDataModule(pl.LightningDataModule):
+    """
+    DataFrameからモデリング時に使用するDataModuleを作成
+    """
+
     def __init__(
         self,
         train_df,
         valid_df,
-        batch_size,
-        max_length,
-        text_column_name: str = "text",
-        label_column_name: str = "label",
-        pretrained_model="cl-tohoku/bert-base-japanese-whole-word-masking",
+        test_df,
+        batch_size=16,
+        max_token_len=512,
+        pretrained_model="cl-tohoku/bert-base-japanese-char-whole-word-masking",
     ):
         super().__init__()
         self.train_df = train_df
         self.valid_df = valid_df
+        self.test_df = test_df
         self.batch_size = batch_size
-        self.max_length = max_length
-        self.tokenizer = BertTokenizer.from_pretrained(pretrained_model)
-        self.text_columm_name = text_column_name
-        self.label_column_name = label_column_name
+        self.max_token_len = max_token_len
+        self.tokenizer = BertJapaneseTokenizer.from_pretrained(pretrained_model)
 
-    def setup(self,stage=None):
-        self.train_dataset = BertDataset(
-            self.train_df,
-            self.tokenizer,
-            self.max_length,
-            self.text_columm_name,
-            self.label_column_name,
+    # train,val,testデータのsplit
+    def setup(self):
+        self.train_dataset = CreateDataset(
+            self.train_df, self.tokenizer, self.max_token_len
         )
-        self.vaild_dataset = BertDataset(
-            self.valid_df,
-            self.tokenizer,
-            self.max_length,
-            self.text_columm_name,
-            self.label_column_name,
+        self.vaild_dataset = CreateDataset(
+            self.valid_df, self.tokenizer, self.max_token_len
+        )
+        self.test_dataset = CreateDataset(
+            self.test_df, self.tokenizer, self.max_token_len
         )
 
+    #
     def train_dataloader(self):
         return DataLoader(
             self.train_dataset,
@@ -102,86 +100,105 @@ class CreateDataModule(pl.LightningDataModule):
             self.vaild_dataset, batch_size=self.batch_size, num_workers=os.cpu_count()
         )
 
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset, batch_size=self.batch_size, num_workers=os.cpu_count()
+        )
 
-class CustumBert(pl.LightningModule):
+
+# Model
+class TextClassifierModel(pl.LightningModule):
     def __init__(
         self,
-        n_classes: int,
-        d_model: int,
-        learning_rate: float,
-        max_length: int,
-        pretrained_model="cl-tohoku/bert-base-japanese-whole-word-masking",
+        n_classes: int,  # クラスの数
+        n_epochs=None,
+        pretrained_model="cl-tohoku/bert-base-japanese-char-whole-word-masking",
     ):
         super().__init__()
 
-        self.bert = BertModel.from_pretrained(pretrained_model)
-        self.classifier = nn.Linear(d_model, n_classes)
-        self.lr = learning_rate
+        # モデルの構造
+        self.bert = BertModel.from_pretrained(pretrained_model, return_dict=True)
+        self.classifier = nn.Linear(self.bert.config.hidden_size, n_classes)
+        self.n_epochs = n_epochs
         self.criterion = nn.CrossEntropyLoss()
-        self.n_classes = n_classes
 
+        # BertLayerモジュールの最後を勾配計算ありに変更
         for param in self.bert.parameters():
             param.requires_grad = False
-
         for param in self.bert.encoder.layer[-1].parameters():
             param.requires_grad = True
 
-        for param in self.classifier.parameters():
-            param.requires_grad = True
+    # 順伝搬
+    def forward(self, input_ids, attention_mask, labels=None):
+        output = self.bert(input_ids, attention_mask=attention_mask)
+        preds = self.classifier(output.pooler_output)
+        loss = 0
+        if labels is not None:
+            loss = self.criterion(preds, labels)
+        return loss, preds
 
-    def forward(self, inputs):
-        outputs = self.bert(inputs)[0]
-        preds = self.classifier(outputs[:, 0, :])
-        return preds
-
+    # trainのミニバッチに対して行う処理
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self.forward(inputs=x)
-        loss = self.criterion(y_hat, y)
-        return {"loss": loss, "batch_preds": y_hat, "batch_labels": y}
+        loss, preds = self.forward(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            labels=batch["labels"],
+        )
+        return {"loss": loss, "batch_preds": preds, "batch_labels": batch["labels"]}
 
+    # validation、testでもtrain_stepと同じ処理を行う
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self.forward(inputs=x)
-        loss = self.criterion(y_hat, y)
-        return {"loss": loss, "batch_preds": y_hat, "batch_labels": y}
+        loss, preds = self.forward(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            labels=batch["labels"],
+        )
+        return {"loss": loss, "batch_preds": preds, "batch_labels": batch["labels"]}
 
-    def on_training_epoch_end(self, outputs, mode="train"):
-        epoch_y_hats = torch.cat([x["batch_preds"] for x in outputs])
+    def test_step(self, batch, batch_idx):
+        loss, preds = self.forward(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            labels=batch["labels"],
+        )
+        return {"loss": loss, "batch_preds": preds, "batch_labels": batch["labels"]}
+
+    # epoch終了時にvalidationのlossとaccuracyを記録
+    def validation_epoch_end(self, outputs, mode="val"):
+        # loss計算
+        epoch_preds = torch.cat([x["batch_preds"] for x in outputs])
         epoch_labels = torch.cat([x["batch_labels"] for x in outputs])
-        epoch_loss = self.criterion(epoch_y_hats, epoch_labels)
-        self.log(f"{mode}_loss", epoch_loss)
+        epoch_loss = self.criterion(epoch_preds, epoch_labels)
+        self.log(f"{mode}_loss", epoch_loss, logger=True)
 
-        _, epoch_preds = torch.max(epoch_y_hats, 1)
-        epoch_accuracy = accuracy(epoch_preds, epoch_labels)
-        self.log(f"{mode}_accuracy", epoch_accuracy)
+        # accuracy計算
+        num_correct = (epoch_preds.argmax(dim=1) == epoch_labels).sum().item()
+        epoch_accuracy = num_correct / len(epoch_labels)
+        self.log(f"{mode}_accuracy", epoch_accuracy, logger=True)
 
-        epoch_auroc = auroc(epoch_y_hats, epoch_labels, num_classes=self.n_classes)
-        self.log(f"{mode}_auroc", epoch_auroc)
+    # testデータのlossとaccuracyを算出（validationの使いまわし）
+    def test_epoch_end(self, outputs):
+        return self.validation_epoch_end(outputs, "test")
 
-    def on_validation_epoch_end(self, outputs, mode="val"):
-        epoch_y_hats = torch.cat([x["batch_preds"] for x in outputs])
-        epoch_labels = torch.cat([x["batch_labels"] for x in outputs])
-        epoch_loss = self.criterion(epoch_y_hats, epoch_labels)
-        self.log(f"{mode}_loss", epoch_loss)
-
-        _, epoch_preds = torch.max(epoch_y_hats, 1)
-        epoch_accuracy = accuracy(epoch_preds, epoch_labels)
-        self.log(f"{mode}_accuracy", epoch_accuracy)
-
-        epoch_auroc = auroc(epoch_y_hats, epoch_labels, num_classes=self.n_classes)
-        self.log(f"{mode}_auroc", epoch_auroc)
-
+    # optimizerの設定
     def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=self.lr)
+        # pretrainされているbert最終層のlrは小さめ、pretrainされていない分類層のlrは大きめに設定
+        optimizer = optim.Adam(
+            [
+                {"params": self.bert.encoder.layer[-1].parameters(), "lr": 5e-5},
+                {"params": self.classifier.parameters(), "lr": 1e-4},
+            ]
+        )
+
+        return [optimizer]
 
 
+# モデルの保存と更新のための関数
 def make_callbacks(min_delta, patience, checkpoint_path):
-
     checkpoint_callback = ModelCheckpoint(
         dirpath=checkpoint_path,
         filename="{epoch}",
-        save_top_k=3,
+        # save_top_k=1,  #save_best_only
         verbose=True,
         monitor="val_loss",
         mode="min",
@@ -194,9 +211,12 @@ def make_callbacks(min_delta, patience, checkpoint_path):
     return [early_stop_callback, checkpoint_callback]
 
 
+# Train Runner
 @hydra.main(config_path=".", config_name="config")
 def main(cfg: DictConfig):
     cwd = hydra.utils.get_original_cwd()
+
+    # wandbの初期化
     wandb_logger = WandbLogger(
         name=("exp_" + str(cfg.wandb.exp_num)),
         project=cfg.wandb.project,
@@ -207,34 +227,46 @@ def main(cfg: DictConfig):
         wandb_logger.experiment.dir, cfg.path.checkpoint_path
     )
     wandb_logger.log_hyperparams(cfg)
-    df = pd.read_csv(cfg.path.data_file_name, sep="\t").dropna().reset_index(drop=True)
-    df[cfg.training.label_column_name] = np.argmax(df.iloc[:, 2:].values, axis=1)
-    df[[cfg.training.text_column_name, cfg.training.label_column_name]]
-    train, test = train_test_split(df, test_size=cfg.training.test_size, shuffle=True)
+
+    # dataModuleのインスタンス化
+    train, val_test = train_test_split(
+        pd.read_csv(cfg.data_file_name),
+        train_size=cfg.traning.val_test_size,
+        random_state=cfg.traning.seed,
+    )
+    val, test = train_test_split(
+        val_test,
+        train_size=cfg.traning.test_size,
+        random_state=cfg.traning.seed,
+    )
     data_module = CreateDataModule(
         train,
+        val,
         test,
-        cfg.training.batch_size,
-        cfg.model.max_length,
-        cfg.training.text_column_name,
+        # cfg.training.batch_size,
+        # cfg.model.max_length,
     )
     data_module.setup()
 
+    # callbackのインスタンス化
     call_backs = make_callbacks(
         cfg.callbacks.patience_min_delta, cfg.callbacks.patience, checkpoint_path
     )
-    model = CustumBert(
+
+    # modelのインスタンスの作成
+    model = TextClassifierModel(
         n_classes=cfg.model.n_classes,
-        d_model=cfg.model.d_model,
-        learning_rate=cfg.training.learning_rate,
-        max_length=cfg.model.max_length,
+        n_epochs=cfg.n_epochs,
     )
+
+    # Trainerの設定
     trainer = pl.Trainer(
         max_epochs=cfg.training.n_epochs,
-        devices=1, 
-        enable_progress_bar=30,
+        devices="auto",
+        # progress_bar_refresh_rate=30,
         callbacks=call_backs,
         logger=wandb_logger,
+        fast_dev_run=True,
     )
     trainer.fit(model, data_module)
 
