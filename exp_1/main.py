e983854b8d15f9ec2d2d0ac6373524552eba3ae6
tnkryoga <tanaryo-miyabi818@outlook.jp>
2023-06-02 10:31:04 +0900

    fix:update on_val,test

--- exp_1/main.py
+++ exp_1/main.py
@@ -172,11 +172,13 @@ class TextClassifierModel(pl.LightningModule):
         self, mode="val"
     ):  # https://github.com/Lightning-AI/lightning/pull/16520
         # loss計算
+        print("batch_preds".size())
+        print(self.validation_step_outputs.size())
         epoch_preds = torch.cat(
-            [x["batch_preds"] for x in self.validation_step_outputs], dim=0
+            [x["batch_preds"] for x in self.validation_step_outputs],
         )
         epoch_labels = torch.cat(
-            [x["batch_labels"] for x in self.validation_step_outputs], dim=0
+            [x["batch_labels"] for x in self.validation_step_outputs],
         )
         epoch_loss = self.criterion(epoch_preds, epoch_labels)
         self.log(f"{mode}_loss", epoch_loss, logger=True)
