278c3d3f7ae6a162fd1d7a12b75ce44326cf340e
tnkryoga <tanaryo-miyabi818@outlook.jp>
2023-12-07 13:55:27 +0900

    fix:metrics

--- exp4/main.py
+++ exp4/main.py
@@ -173,9 +173,9 @@ class MaltiLabelClassifierModel(pl.LightningModule):
                 torchmetrics.F1Score(
                     task="multilabel", num_labels=16, threshold=self.THRESHOLD
                 ),
-                torchmetrics.MatthewsCorrCoef(
+                """torchmetrics.MatthewsCorrCoef(
                     task="multilabel", num_labels=16, threshold=self.THRESHOLD
-                ),
+                ),""",
             ]
         )
 
@@ -368,10 +368,15 @@ class MaltiLabelClassifierModel(pl.LightningModule):
         for i in range(self.num_classes):
             label_preds = epoch_preds[:, i]  # i番目の要素のみを抽出
             label_labels = epoch_labels[:, i]
+            n = 0
+            for j in range(len(label_labels)):
+                if label_labels[j] == 1:
+                    n += 1
+            print("正解データに{j}クラスがいくつあるかの確認：" + n)
             preds_binary = np.where(label_preds > self.THRESHOLD, 1, 0)
             wandb.log(
                 {
-                    f"test/confusion_matrix_{i}": plot.roc_curve(
+                    f"test/confusion_matrix_{i}": plot.confusion_matrix(
                         y_true=label_labels,
                         y_probas=self.complement_score(preds_binary),
                         labels=["応答なし", "応答あり"],
