bec06470b1e5d335515bd0b6f973c1f7c9d4cb5b
tnkryoga <tanaryo-miyabi818@outlook.jp>
2024-01-17 11:27:18 +0900

    fix:hidden_layer

--- exp6/main.py
+++ exp6/main.py
@@ -263,9 +263,7 @@ class MaltiLabelClassifierModel(pl.LightningModule):
     def forward(self, input_ids, attention_mask, labels=None):
         output = self.bert(input_ids, attention_mask=attention_mask)
         hidden_outputs = []
-        for classifier, hidden_layer1, hidden_layer2 in zip(
-            self.classifiers, self.hidden_layer1, self.hidden_layer2
-        ):
+        for classifier, hidden_layer1 in zip(self.classifiers, self.hidden_layer1):
             binary_output = torch.relu(classifier(output.pooler_output))
             hidden_output1 = torch.relu(hidden_layer1(binary_output))
             # hidden_output2 = torch.relu(hidden_layer2(hidden_output1))
